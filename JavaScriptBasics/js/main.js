8da10a501e0f9428bff751264e66217dd27a7a00
nogu66 <ezochi66@gmail.com>
2023-02-09 20:51:02 +0900

    分割代入

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -1,7 +1,24 @@
 'use strict';
 
 {
-  const otherScores = [10, 20];
-  const scores = [80, 90, 40, 70, ...otherScores];
-  console.log(scores);
+  const scores = [80, 90, 40, 70];
+
+  //   const [a, b, c, d] = scores;
+  //   console.log(a);
+  //   console.log(b);
+  //   console.log(c);
+  //   console.log(d);
+
+  const [a, b, ...others] = scores;
+  console.log(a);
+  console.log(b);
+  console.log(others);
+
+  let x = 30;
+  let y = 70;
+  console.log(x);
+  console.log(y);
+  [x, y] = [y, x];
+  console.log(x);
+  console.log(y);
 }
