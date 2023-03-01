a5d82b8c13d1489c0e0268627ee75586da5e709c
nogu66 <ezochi66@gmail.com>
2023-03-01 20:21:05 +0900

    jsBasic

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -1,10 +1,46 @@
 'use strict';
 {
-  const answer = confirm('削除しますか');
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
 
-  if (answer) {
-    console.log('削除しました');
-  } else {
-    console.log('キャンセルしました');
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
+
+    likes() {
+      this.likeCount++;
+    }
+
+    static showInfo() {
+      console.log('Post class version 1.0');
+    }
   }
+
+  class SponsoredPost extends Post {
+    constructor(text, sponsor) {
+      super(text);
+      this.sponsor = sponsor;
+    }
+
+    show() {
+      super.show();
+      console.log(`... sponsored by ${this.sponsor}`);
+    }
+
+    static showInfo() {
+      console.log('Post class version 1.0');
+    }
+  }
+
+  const posts = [
+    new Post('JavaScriptの勉強中'),
+    new Post('プログラミング楽しい！'),
+    new SponsoredPost('3分動画でマスターしよう', 'dotinstall'),
+  ];
+
+  posts[2].show();
+  posts[2].likes();
 }
